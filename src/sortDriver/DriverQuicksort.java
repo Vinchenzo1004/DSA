80cc2d5ae90bcf70ee013d41fbfbe7c1edc0f9b9
Vinchenzo1004 <vinchenzo1004@gmail.com>
2024-03-06 14:33:19 -0500

    Week 8 Lab

--- src/sortDriver/DriverQuicksort.java
+++ src/sortDriver/DriverQuicksort.java
@@ -75,13 +75,13 @@ public class DriverQuicksort
         System.out.println();
 
         numbers.clear();
-        numbers.add(7);
-        numbers.add(6);
-        numbers.add(5);
-        numbers.add(4);
-        numbers.add(3);
         numbers.add(2);
+        numbers.add(6);
+        numbers.add(4);
         numbers.add(1);
+        numbers.add(3);
+        numbers.add(5);
+        numbers.add(7);
         System.out.println("Before sorting:");
         System.out.println(numbers);
         sorter.sort(numbers);
